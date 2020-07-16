3349c83bd3e7c0c57ea7a11a8f8857ad0cda2134
Dmitrii Sugrobov <dmitrii.sugrobov@leroymerlin.ru>
2020-07-16 17:35:33 +0300

    russian lang by default

--- assets/js/base.js
+++ assets/js/base.js
@@ -74,5 +74,56 @@ limitations under the License.
         });
     });
 
-
+    
 }(jQuery));
+
+    (function () {
+        var getFirstBrowserLanguage = function () {
+        var nav = window.navigator,
+        browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+        i,
+        language
+
+        if (Array.isArray(nav.languages)) {
+          for (i = 0; i < nav.languages.length; i++) {
+            language = nav.languages[i]
+            if (language && language.length) {
+              return language
+            }
+          }
+        }
+
+        // support for other well known properties in browsers
+        for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+          language = nav[browserLanguagePropertyKeys[i]]
+          if (language && language.length) {
+            return language
+          }
+        }
+        return 'en'
+      }
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+      var preferLang = getFirstBrowserLanguage()
+      if (window.location.pathname == '/' && !getCookie("redirected") && preferLang.indexOf('en') !== -1) {
+        setCookie("redirected", true, 365);
+        window.location.replace('/en/');
+      }
+    })()
